758178cdbb6bb54c1b48c29a5151c9c8cd46a3cf
Balti2003 <baltasarlomello@live.com>
2025-12-23 15:25:44 -0300

    creando modelos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 const app = express();
+const DB_URL = process.env.NODE_ENV === 'test' 
+    ? 'mongodb://localhost:27017/ticketing-db-test' 
+    : process.env.DB_URL || 'mongodb://localhost:27017/ticketing-db';
 
+mongoose.connect(DB_URL) // Conexión a la base de datos mongo db
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+    
 app.use(morgan('dev')); // middleware para logging de peticiones
 app.use(express.json()); // middleware para parsear JSON en las peticiones
 
